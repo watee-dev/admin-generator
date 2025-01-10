206a6af3e48c796d02e0ffbeaf0b855e9858c08d
watee-dev <dev@watee.sk>
2025-01-10 17:03:00 +0100

    fix: override

--- src/Generate/ClassGenerator.php
+++ src/Generate/ClassGenerator.php
@@ -154,9 +154,9 @@ abstract class ClassGenerator extends Command
 	 *
 	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
 	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-	 * @return mixed
 	 */
-	public function execute(InputInterface $input, OutputInterface $output): int
+	#[\Override]
+	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
 		if ($this instanceof Model) {
 			$this->initCommonNames($this->argument('table_name'), $this->argument('class_name'), null, $this->option('model-with-full-namespace'));
