9cbf5219e50d59122ad406d284dd00ee26949664
watee-dev <dev@watee.sk>
2025-01-10 17:10:32 +0100

    fix: file appender execute

--- src/Generate/FileAppender.php
+++ src/Generate/FileAppender.php
@@ -1,4 +1,6 @@
-<?php namespace Brackets\AdminGenerator\Generate;
+<?php
+
+namespace Brackets\AdminGenerator\Generate;
 
 use Brackets\AdminGenerator\Generate\Traits\Helpers;
 use Brackets\AdminGenerator\Generate\Traits\Names;
@@ -9,99 +11,101 @@ use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class FileAppender extends Command {
+abstract class FileAppender extends Command
+{
 
-    use Helpers, Columns, Names;
+	use Helpers, Columns, Names;
 
-    /**
-     * @var Filesystem
-     */
-    protected $files;
+	/**
+	 * @var Filesystem
+	 */
+	protected $files;
 
-    /**
-     * Relations
-     *
-     * @var string
-     */
-    protected $relations = [];
+	/**
+	 * Relations
+	 *
+	 * @var string
+	 */
+	protected $relations = [];
 
-    /**
-     * Create a new controller creator command instance.
-     *
-     * @param  \Illuminate\Filesystem\Filesystem  $files
-     */
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct();
+	/**
+	 * Create a new controller creator command instance.
+	 *
+	 * @param  \Illuminate\Filesystem\Filesystem  $files
+	 */
+	public function __construct(Filesystem $files)
+	{
+		parent::__construct();
 
-        $this->files = $files;
-    }
+		$this->files = $files;
+	}
 
-    protected function getArguments() {
-        return [
-            ['table_name', InputArgument::REQUIRED, 'Name of the existing table'],
-        ];
-    }
+	protected function getArguments()
+	{
+		return [
+			['table_name', InputArgument::REQUIRED, 'Name of the existing table'],
+		];
+	}
 
-    /**
-     * Append content to file only if if the content is not present in the file
-     *
-     * @param $path
-     * @param $content
-     * @param string $defaultContent content that will be used to populated with newly created file (in case it does not already exists)
-     * @return bool
-     */
-    protected function appendIfNotAlreadyAppended($path, $content, $defaultContent = "<?php".PHP_EOL.PHP_EOL)
-    {
-        if (!$this->files->exists($path)) {
-            $this->makeDirectory($path);
-            $this->files->put($path, $defaultContent.$content);
-        } else if (!$this->alreadyAppended($path, $content)) {
-            $this->files->append($path, $content);
-        } else {
-            return false;
-        }
+	/**
+	 * Append content to file only if if the content is not present in the file
+	 *
+	 * @param $path
+	 * @param $content
+	 * @param string $defaultContent content that will be used to populated with newly created file (in case it does not already exists)
+	 * @return bool
+	 */
+	protected function appendIfNotAlreadyAppended($path, $content, $defaultContent = "<?php" . PHP_EOL . PHP_EOL)
+	{
+		if (!$this->files->exists($path)) {
+			$this->makeDirectory($path);
+			$this->files->put($path, $defaultContent . $content);
+		} else if (!$this->alreadyAppended($path, $content)) {
+			$this->files->append($path, $content);
+		} else {
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * Append content to file only if if the content is not present in the file
-     *
-     * @param $path
-     * @param $search
-     * @param $replace
-     * @param string $defaultContent content that will be used to populated with newly created file (in case it does not already exists)
-     * @return bool
-     */
-    protected function replaceIfNotPresent($path, $search, $replace, $defaultContent = "<?php".PHP_EOL.PHP_EOL)
-    {
-        if (!$this->files->exists($path)) {
-            $this->makeDirectory($path);
-            $this->files->put($path, $defaultContent);
-        }
+	/**
+	 * Append content to file only if if the content is not present in the file
+	 *
+	 * @param $path
+	 * @param $search
+	 * @param $replace
+	 * @param string $defaultContent content that will be used to populated with newly created file (in case it does not already exists)
+	 * @return bool
+	 */
+	protected function replaceIfNotPresent($path, $search, $replace, $defaultContent = "<?php" . PHP_EOL . PHP_EOL)
+	{
+		if (!$this->files->exists($path)) {
+			$this->makeDirectory($path);
+			$this->files->put($path, $defaultContent);
+		}
 
-        if (!$this->alreadyAppended($path, $replace)) {
-            $this->files->put($path, str_replace($search, $replace, $this->files->get($path)));
-            return true;
-        } else {
-            return false;
-        }
-    }
+		if (!$this->alreadyAppended($path, $replace)) {
+			$this->files->put($path, str_replace($search, $replace, $this->files->get($path)));
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return mixed
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->initCommonNames($this->argument('table_name'), $this->option('model-name'), $this->option('controller-name'), $this->option('model-with-full-namespace'));
+	/**
+	 * Execute the console command.
+	 *
+	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
+	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 */
+	#[\Override]
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$this->initCommonNames($this->argument('table_name'), $this->option('model-name'), $this->option('controller-name'), $this->option('model-with-full-namespace'));
 
-        $output = parent::execute($input, $output);
+		$output = parent::execute($input, $output);
 
-        return $output;
-    }
+		return $output;
+	}
 }
