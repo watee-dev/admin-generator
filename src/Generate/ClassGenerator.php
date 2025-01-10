45ab8fe7439c6d8bb255341e6f5563eb6a2f587b
watee-dev <dev@watee.sk>
2025-01-10 16:56:48 +0100

    fix: laravel 11 class generator fix

--- src/Generate/ClassGenerator.php
+++ src/Generate/ClassGenerator.php
@@ -1,4 +1,6 @@
-<?php namespace Brackets\AdminGenerator\Generate;
+<?php
+
+namespace Brackets\AdminGenerator\Generate;
 
 use Brackets\AdminGenerator\Generate\Traits\Helpers;
 use Brackets\AdminGenerator\Generate\Traits\Names;
@@ -11,168 +13,172 @@ use Illuminate\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class ClassGenerator extends Command {
+abstract class ClassGenerator extends Command
+{
 
-    use Helpers, Columns, Names;
+	use Helpers, Columns, Names;
 
-    public $classBaseName;
-    public $classFullName;
-    public $classNamespace;
+	public $classBaseName;
+	public $classFullName;
+	public $classNamespace;
 
-    /**
-     * @var Filesystem
-     */
-    protected $files;
+	/**
+	 * @var Filesystem
+	 */
+	protected $files;
 
-    /**
-     * Relations
-     *
-     * @var string
-     */
-    protected $relations = [];
+	/**
+	 * Relations
+	 *
+	 * @var string
+	 */
+	protected $relations = [];
 
-    /**
-     * Create a new controller creator command instance.
-     *
-     * @param  \Illuminate\Filesystem\Filesystem  $files
-     */
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct();
+	/**
+	 * Create a new controller creator command instance.
+	 *
+	 * @param  \Illuminate\Filesystem\Filesystem  $files
+	 */
+	public function __construct(Filesystem $files)
+	{
+		parent::__construct();
 
-        $this->files = $files;
-    }
+		$this->files = $files;
+	}
 
-    protected function getArguments() {
-        return [
-            ['table_name', InputArgument::REQUIRED, 'Name of the existing table'],
-            ['class_name', InputArgument::OPTIONAL, 'Name of the generated class'],
-        ];
-    }
+	protected function getArguments()
+	{
+		return [
+			['table_name', InputArgument::REQUIRED, 'Name of the existing table'],
+			['class_name', InputArgument::OPTIONAL, 'Name of the generated class'],
+		];
+	}
 
-    /**
-     * Generate default class name (for case if not passed as argument) from table name
-     *
-     * @param $tableName
-     * @return mixed
-     */
-    abstract public function generateClassNameFromTable($tableName);
+	/**
+	 * Generate default class name (for case if not passed as argument) from table name
+	 *
+	 * @param $tableName
+	 * @return mixed
+	 */
+	abstract public function generateClassNameFromTable($tableName);
 
-    /**
-     * Build the class with the given name.
-     *
-     * @return string
-     */
-    abstract protected function buildClass();
+	/**
+	 * Build the class with the given name.
+	 *
+	 * @return string
+	 */
+	abstract protected function buildClass();
 
-    public function getPathFromClassName($name) {
-        $path = str_replace('\\', '/', $name).".php";
+	public function getPathFromClassName($name)
+	{
+		$path = str_replace('\\', '/', $name) . ".php";
 
-        return preg_replace('|^App/|', 'app/', $path);
-    }
+		return preg_replace('|^App/|', 'app/', $path);
+	}
 
-    /**
-     * Get the full namespace for a given class, without the class name.
-     *
-     * @param  string  $name
-     * @return string
-     */
-    protected function getNamespace($name)
-    {
-        return trim(implode('\\', array_slice(explode('\\', $name), 0, -1)), '\\');
-    }
+	/**
+	 * Get the full namespace for a given class, without the class name.
+	 *
+	 * @param  string  $name
+	 * @return string
+	 */
+	protected function getNamespace($name)
+	{
+		return trim(implode('\\', array_slice(explode('\\', $name), 0, -1)), '\\');
+	}
 
-    /**
-     * Get the root namespace for the class.
-     *
-     * @return string
-     */
-    public function rootNamespace()
-    {
-        return $this->laravel->getNamespace();
-    }
+	/**
+	 * Get the root namespace for the class.
+	 *
+	 * @return string
+	 */
+	public function rootNamespace()
+	{
+		return $this->laravel->getNamespace();
+	}
 
-    /**
-     * Parse the class name and format according to the root namespace.
-     *
-     * @param  string  $name
-     * @return string
-     */
-    public function qualifyClass($name)
-    {
-        $name = str_replace('/', '\\', $name);
+	/**
+	 * Parse the class name and format according to the root namespace.
+	 *
+	 * @param  string  $name
+	 * @return string
+	 */
+	public function qualifyClass($name)
+	{
+		$name = str_replace('/', '\\', $name);
 
-        $rootNamespace = $this->rootNamespace();
+		$rootNamespace = $this->rootNamespace();
 
-        if (Str::startsWith($name, $rootNamespace)) {
-            return $name;
-        }
+		if (Str::startsWith($name, $rootNamespace)) {
+			return $name;
+		}
 
-        return $this->qualifyClass(
-            $this->getDefaultNamespace(trim($rootNamespace, '\\')).'\\'.$name
-        );
-    }
+		return $this->qualifyClass(
+			$this->getDefaultNamespace(trim($rootNamespace, '\\')) . '\\' . $name
+		);
+	}
 
-    /**
-     * Get the default namespace for the class.
-     *
-     * @param  string  $rootNamespace
-     * @return string
-     */
-    protected function getDefaultNamespace($rootNamespace)
-    {
-        return $rootNamespace;
-    }
+	/**
+	 * Get the default namespace for the class.
+	 *
+	 * @param  string  $rootNamespace
+	 * @return string
+	 */
+	protected function getDefaultNamespace($rootNamespace)
+	{
+		return $rootNamespace;
+	}
 
-    protected function generateClass($force = false) {
-        $path = base_path($this->getPathFromClassName($this->classFullName));
+	protected function generateClass($force = false)
+	{
+		$path = base_path($this->getPathFromClassName($this->classFullName));
 
-        if ($this->alreadyExists($path)) {
-            if($force) {
-                $this->warn('File '.$path.' already exists! File will be deleted.');
-                $this->files->delete($path);
-            } else {
-                $this->error('File '.$path.' already exists!');
-                return false;
-            }
-        }
+		if ($this->alreadyExists($path)) {
+			if ($force) {
+				$this->warn('File ' . $path . ' already exists! File will be deleted.');
+				$this->files->delete($path);
+			} else {
+				$this->error('File ' . $path . ' already exists!');
+				return false;
+			}
+		}
 
-        $this->makeDirectory($path);
+		$this->makeDirectory($path);
 
-        $this->files->put($path, $this->buildClass());
-        return true;
-    }
+		$this->files->put($path, $this->buildClass());
+		return true;
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return mixed
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        if ($this instanceof Model) {
-            $this->initCommonNames($this->argument('table_name'), $this->argument('class_name'), null, $this->option('model-with-full-namespace'));
-        } else {
-            $this->initCommonNames($this->argument('table_name'), $this->option('model-name'), null, $this->option('model-with-full-namespace'));
-        }
+	/**
+	 * Execute the console command.
+	 *
+	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
+	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 * @return mixed
+	 */
+	public function execute(InputInterface $input, OutputInterface $output): int
+	{
+		if ($this instanceof Model) {
+			$this->initCommonNames($this->argument('table_name'), $this->argument('class_name'), null, $this->option('model-with-full-namespace'));
+		} else {
+			$this->initCommonNames($this->argument('table_name'), $this->option('model-name'), null, $this->option('model-with-full-namespace'));
+		}
 
-        $this->initClassNames($this->argument('class_name'));
+		$this->initClassNames($this->argument('class_name'));
 
-        $output = parent::execute($input, $output);
+		$output = parent::execute($input, $output);
 
-        return $output;
-    }
+		return $output;
+	}
 
-    protected function initClassNames($className = null) {
-        if (empty($className)) {
-            $className = $this->generateClassNameFromTable($this->tableName);
-        }
+	protected function initClassNames($className = null)
+	{
+		if (empty($className)) {
+			$className = $this->generateClassNameFromTable($this->tableName);
+		}
 
-        $this->classFullName = $this->qualifyClass($className);
-        $this->classBaseName = class_basename($this->classFullName);
-        $this->classNamespace = Str::replaceLast("\\".$this->classBaseName, '', $this->classFullName);
-    }
-
+		$this->classFullName = $this->qualifyClass($className);
+		$this->classBaseName = class_basename($this->classFullName);
+		$this->classNamespace = Str::replaceLast("\\" . $this->classBaseName, '', $this->classFullName);
+	}
 }
